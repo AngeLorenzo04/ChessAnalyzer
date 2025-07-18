12d7008b8587f1025e675eb323c1666a17111c11
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-19 00:56:38 +0200

    fixed Utils warnings

--- main/src/main/java/org/example/API/ApiUtils.java
+++ main/src/main/java/org/example/API/ApiUtils.java
@@ -1,28 +1,8 @@
 package org.example.API;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 import com.google.gson.annotations.SerializedName;
-
-public class ApiUtils {
-
-    public static void main(String[] args) {
-        ChessAPIService apiService = new ChessAPIService();
-        ChessDataParser dataParser = new ChessDataParser();
-        ChessCLI cli = new ChessCLI();
-
-        try {
-            CompletableFuture<ChessArchive> selectedArchive = cli.selectArchive();
-            String gamesJson = apiService.getGamesFromArchive(selectedArchive.get().getUrl());
-            List<ChessGame> games = dataParser.parseGames(gamesJson);
-
-            cli.displayPGN();
-        } catch (Exception e) {
-            cli.displayError();
-        }
-    }
-}
 
 // Modelli dati per il parsing JSON
 class ArchivesResponse {
@@ -38,17 +18,19 @@ class GamesResponse {
     @SerializedName("games")
     private List<GameData> gameData;
 
+    GamesResponse(List<GameData> gameData) {
+        this.gameData = gameData;
+    }
+
     public List<ChessGame> getGames() {
         return gameData.stream()
-                .map(data -> new ChessGame(data.pgn, data.end_time))
+                .map(data -> new ChessGame(data.pgn))
                 .toList();
     }
 
     private static class GameData {
         @SerializedName("pgn")
         String pgn;
-        @SerializedName("end_time")
-        long end_time;
     }
 }
 
